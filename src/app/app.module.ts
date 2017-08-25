3084afbb28c9987e47e99ffc4fec9dbf73288b5d
Rushaine McBean <rmcbean@businessinsider.com>
2017-08-25 14:14:37 -0400

    Adds remaining prod components to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductRowComponent } from './product-row/product-row.component';
 import { ProductImageComponent } from './product-image/product-image.component';
 import { ProductDisplayComponent } from './product-display/product-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,13 @@ import { ProductDisplayComponent } from './product-display/product-display.compo
     ProductsListComponent,
     ProductRowComponent,
     ProductImageComponent,
-    ProductDisplayComponent
+    ProductDisplayComponent,
+    ProductDepartmentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
